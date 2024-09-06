2aa05ff4b68430edf5827a50ba5e669106a49e83
NICOLAS ANDRÉ KRAUSE DE MELLO <nicolas_a_mello@estudante.sesisenai.org.br>
2024-09-06 12:20:58 -0300

    1.0

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 import { fastify } from 'fastify'
-import { DatabaseUsers } from './database.js'
+import { Database } from './database-users.js';
 
 const server = fastify();
-const database = new DatabaseUsers;
+const database = new Database;
 
-server.post('/users', async (request, reply) => {
+server.post('/users',async (request,reply) => {
   const body = request.body;
   await database.createUser(body);
-  return 201
-})
-
-server.get('/', () => {
-  return 'Brunna';
+  
+  return 201;
 });
 
 server.listen({
-  port: 3333,
+  port: 1224,
 })
